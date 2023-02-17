bd84283489fddd662b4d5ee2ac19a718b2474de9
Sarb Bhinder <sarb8275@gmail.com>
2023-02-17 02:01:41 -0500

    Loading all services feature started

---
+++ src/components/ListAllServices/ListAllServices.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+
+class ListAllService extends Component{
+
+    constructor(props){
+        super(props);
+
+    }
+    getAllServices(){
+
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ListAllService;
